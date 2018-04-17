c6bfd06309f8a0e8c69af797076269ee64428da7
Will Mitchell <wvmitchell@gmail.com>
2018-04-17 10:13:14 -0600

    update cors to allow all origins

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const app = express();
 const users = require('./routes/usersApi');
 
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
