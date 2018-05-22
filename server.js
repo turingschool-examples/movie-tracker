aae00400f96cb20137ca145eb863ff4637bab33c
Will Mitchell <wvmitchell@gmail.com>
2018-05-22 11:05:34 -0600

    cors should allow all method types

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const users = require('./routes/usersApi');
 app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
